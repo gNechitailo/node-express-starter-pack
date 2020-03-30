79ec20b50af87d8244a5052af35de0b101fc0a1a
Grigorii Nechitailo <g.nechitailo@dunice.net>
2020-03-30 12:55:01 +0300

    fixed assertions

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 const assert = require('assert');
 
-assert(process.env.DB_USER, 'DB_USER is not specified');
-assert(process.env.DB_PASS, 'DB_PASS is not specified');
-assert(process.env.DB_NAME, 'DB_NAME is not specified');
+assert(process.env.POSTGRES_USER, 'POSTGRES_USER is not specified');
+assert(process.env.POSTGRES_PASSWORD, 'POSTGRES_PASSWORD is not specified');
+assert(process.env.POSTGRES_DB, 'POSTGRES_DB is not specified');
 assert(process.env.DB_HOST, 'DB_HOST is not specified');
 
 module.exports = {
